fccd47854b8bff1f0b2afc2eaaf2bad341cc5dc4
bdafahim <badrudduza20@student.sust.edu>
2021-12-22 01:24:43 +0600

    skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {appRoutes} from './app.routes';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router';
+
+export const appRoutes: Routes = [
+  // {
+  //   path: 'ed/entity-chart',
+  //   loadChildren:
+  //     () => import('./features/entity-designer/orbitax-entity-designer.module').then(m => m.OrbitaxEntityDesignerModule),
+  // },
+];
